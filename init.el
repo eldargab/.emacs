40d0736fcb6f28398aa6fb5bac273a399531ba54
Eldar Gabdullin <eldargab@gmail.com>
2015-04-22 00:01:25 +0300

    refactor key bindings

--- init.el
+++ init.el
@@ -4,6 +4,7 @@
       (setenv "PATH" path)))
 
 (require 'package)
+
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
 
@@ -17,39 +18,6 @@
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
-
-
-;; Ido setup
-(ido-mode t)
-(setq ido-ignore-extensions t)
-(setq ido-enable-flex-matching t)
-
-(add-to-list 'ido-ignore-buffers "\\`*")
-(add-to-list 'ido-ignore-buffers "\.gz")
-
-(add-hook 'dired-mode-hook 'ensure-buffer-name-ends-in-slash)
-(defun ensure-buffer-name-ends-in-slash ()
-  "change buffer name to end with slash"
-  (let ((name (buffer-name)))
-    (if (not (string-match "/$" name))
-        (rename-buffer (concat name "/") t))))
-
-
-(when (fboundp 'tool-bar-mode)
-  (tool-bar-mode -1))
-
-(when (fboundp 'scroll-bar-mode)
-  (scroll-bar-mode -1))
-
-(require 'uniquify)
-(setq uniquify-buffer-name-style 'forward)
-
-(require 'saveplace)
-(setq-default save-place t)
-
-(require 'smart-tab)
-(global-smart-tab-mode 1)
-(setq dabbrev-check-all-buffers nil)
 
 (show-paren-mode 1)
 (delete-selection-mode 1)
@@ -65,48 +33,121 @@
       ediff-window-setup-function 'ediff-setup-windows-plain
       save-place-file (concat user-emacs-directory "places")
       backup-directory-alist `(("." . ,(concat user-emacs-directory
-                                               "backups"))))
-
-;; customize scrolling
-(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))
-      ;; mouse-wheel-progressive-speed 5
+                                               "backups")))
+      mouse-wheel-scroll-amount '(1 ((shift) . 1))
       scroll-step 1)
 
-(setq inferior-lisp-program "/usr/local/bin/ccl")
+(load-file (concat user-emacs-directory "eldar-theme.el"))
 
-(load-file (concat user-emacs-directory "ProofGeneral/generic/proof-site.el"))
+;; Ido setup
+(ido-mode t)
+(setq ido-ignore-extensions t)
+(setq ido-enable-flex-matching t)
 
-(setq coq-prog-name "/usr/local/bin/coqtop")
+(add-to-list 'ido-ignore-buffers "\\`*")
+(add-to-list 'ido-ignore-buffers "\.gz")
+(add-to-list 'ido-ignore-buffers "\.v\.d")
 
-;; Navigation
-(global-set-key (kbd "<C-tab>") 'ido-switch-buffer)
-(global-set-key (kbd "<C-s-tab>") 'ibuffer)
+(add-hook 'dired-mode-hook 'ensure-buffer-name-ends-in-slash)
+(defun ensure-buffer-name-ends-in-slash ()
+  "change buffer name to end with slash"
+  (let ((name (buffer-name)))
+    (if (not (string-match "/$" name))
+        (rename-buffer (concat name "/") t))))
 
-;; (define-key ido-completion-map "\t" nil)
-(global-set-key (kbd "C-q") 'keyboard-escape-quit)
-(global-set-key (kbd "s-o") 'ido-find-file)
-(global-set-key (kbd "s-0") 'delete-window)
-(global-set-key (kbd "s-1") 'delete-other-windows)
-(global-set-key (kbd "s-2") '(lambda ()
-                               (interactive)
-                               (split-window-right)
-                               (windmove-right)))
-(global-set-key (kbd "s-3") '(lambda ()
-                               (interactive)
-                               (split-window-below)
-                               (windmove-down)))
-(global-set-key [s-left] 'windmove-left) 
-(global-set-key [s-right] 'windmove-right) 
-(global-set-key [s-up] 'windmove-up) 
-(global-set-key [s-down] 'windmove-down)
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+
+(require 'smart-tab)
+(global-smart-tab-mode 1)
+
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+(global-unset-key (kbd "C-z"))
+
+;; Windows and navigation
+
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
+
+(require 'saveplace)
+(setq-default save-place t)
 
 (when (fboundp 'winner-mode)
   (winner-mode 1))
 
+(setq dabbrev-check-all-buffers nil)
+
+(defun my-split-window-right ()
+  (interactive) 
+  (split-window-right)
+  (windmove-right))
+
+(defun my-split-window-below ()
+  (interactive)
+  (split-window-below)
+  (windmove-down))
+
 (global-set-key (kbd "M-q") 'winner-undo)
 (global-set-key (kbd "s-M-q") 'winner-redo)
+(global-set-key (kbd "<s-left>") 'windmove-left) 
+(global-set-key (kbd "<s-right>") 'windmove-right) 
+(global-set-key (kbd "<s-up>") 'windmove-up) 
+(global-set-key (kbd "<s-down>") 'windmove-down)
+(global-set-key (kbd "s-0") 'delete-window)
+(global-set-key (kbd "s-1") 'delete-other-windows)
+(global-set-key (kbd "s-2") 'my-split-window-right)
+(global-set-key (kbd "s-3") 'my-split-window-below)
 
-;; commenting
+
+(global-set-key (kbd "s-o") 'ido-find-file)
+(global-set-key (kbd "<C-tab>") 'ido-switch-buffer)
+(global-set-key (kbd "<C-s-tab>") 'ibuffer)
+
+;; Pretty printing
+
+(defun my-indent ()
+  (interactive)
+  (if (region-active-p)
+      (indent-region (region-beginning) (region-end))
+    (indent-region (line-beginning-position) (line-end-position))))
+
+(defun my-indent-file ()
+  (interactive)
+  (indent-region 0 (buffer-size)))
+
+(global-set-key (kbd "s-p") 'my-indent)
+(global-set-key (kbd "s-M-π") 'my-indent-file)
+
+;; General editing commands
+
+(defun my-new-line ()
+  (interactive)
+  (move-end-of-line nil)
+  (newline-and-indent))
+
+(defun my-new-line-above ()
+  (interactive)
+  (previous-line)
+  (move-end-of-line nil)
+  (newline-and-indent))
+
+(defun my-kill-whole-line ()
+  (interactive)
+  (kill-whole-line)
+  (indent-according-to-mode))
+
+(defun my-join-line ()
+  (interactive)
+  (delete-indentation 1))
+
+(global-set-key (kbd "<s-return>") 'my-new-line)
+(global-set-key (kbd "<s-M-return>") 'my-new-line-above)
+(global-set-key (kbd "s-k") 'my-kill-whole-line)
+(global-set-key (kbd "s-j") 'my-join-line)
+
 (defun my-toggle-comment ()
   (interactive)
   (let ((start (line-beginning-position))
@@ -124,46 +165,15 @@
 
 (global-set-key (kbd "s-/") 'my-toggle-comment)
 
-;; indentation
-(defun my-indent ()
-  (interactive)
-  (if (region-active-p)
-      (indent-region (region-beginning) (region-end))
-    (indent-region 0 (buffer-size))))
-
-(defun my-indent-line ()
-  (interactive)
-  (indent-region (line-beginning-position) (line-end-position)))
-
-(global-set-key (kbd "s-p") 'my-indent)
-(global-set-key (kbd "C-p") 'my-indent-line)
-
-;; general sublime like commands
-(defun my-new-line ()
-  (interactive)
-  (move-end-of-line nil)
-  (newline-and-indent))
-
-(defun my-new-line-above ()
-  (interactive)
-  (previous-line)
-  (move-end-of-line nil)
-  (newline-and-indent))
-
-(global-set-key (kbd "<C-return>") 'my-new-line)
-(global-set-key (kbd "<C-M-return>") 'my-new-line-above)
-(global-set-key (kbd "C-k") 'kill-whole-line)
-
-
 ;; eval, compilation and stuf
 (global-set-key (kbd "<f5>") 'eshell)
-
-(setq k-eval (kbd "<s-return>"))
+(setq k-eval (kbd "<M-return>"))
 (setq k-compile (kbd "<f8>"))
 (setq k-docs (kbd "<f4>"))
 (setq k-jump-to-definition (kbd "<double-mouse-1>"))
 (setq k-jump-back (kbd "<s-double-mouse-1>>"))
 (setq k-apropos (kbd "<s-f1>"))
+
 
 ;; elisp
 (defun my-elisp-eval ()
@@ -176,39 +186,9 @@
 (define-key emacs-lisp-mode-map k-compile 'eval-buffer)
 (define-key emacs-lisp-mode-map k-jump-to-definition 'find-function-at-point)
 
-;; Proof General
-(defun move-proof-to-point ()
-  (interactive)
-  (if (> (proof-queue-or-locked-end) (point))
-      (save-excursion
-        (proof-retract-until-point))
-    (progn
-      (save-excursion
-        (if (proof-only-whitespace-to-locked-region-p)
-            (proof-assert-next-command-interactive)
-          (proof-assert-until-point)))
-      (proof-maybe-follow-locked-end))))
-
-(add-hook 'coq-mode-hook
-          '(lambda ()
-             (set (make-local-variable 'electric-indent-chars) '(?\n ?| ?.))
-             (define-key coq-mode-map k-eval 'move-proof-to-point)
-             (define-key coq-mode-map (kbd "<M-s-return>") 'proof-undo-last-successful-command)
-             (define-key coq-mode-map k-compile 'coq-Compile)
-             (define-key coq-mode-map k-jump-to-definition '(lambda ()
-                                                              (interactive)
-                                                              (execute-kbd-macro
-                                                               (kbd "M-x coq-Print RET RET"))))
-             (define-key coq-mode-map k-docs '(lambda ()
-                                                (interactive)
-                                                (execute-kbd-macro
-                                                 (kbd "M-x coq-Check RET RET"))))
-             (define-key coq-mode-map k-apropos 'coq-SearchAbout)))
-
-(setq proof-follow-mode 'followdown)
-(setq proof-splash-enable nil)
-
 ;; Slime
+(setq inferior-lisp-program "/usr/local/bin/ccl")
+
 (defun my-slime-eval ()
   (interactive)
   (if (region-active-p)
@@ -223,7 +203,52 @@
              (define-key slime-mode-map k-jump-to-definition 'slime-edit-definition)
              (define-key slime-mode-map k-jump-back 'slime-pop-find-definition-stack)))
 
-(load-file (concat user-emacs-directory "eldar-theme.el"))
+
+;; Proof General
+(load-file (concat user-emacs-directory "ProofGeneral/generic/proof-site.el"))
+
+(setq coq-prog-name "/usr/local/bin/coqtop")
+
+(add-to-list 'completion-ignored-extensions ".v.d")
+
+(defun move-proof-to-point ()
+  (interactive)
+  (if (> (proof-queue-or-locked-end) (point))
+      (save-excursion
+        (proof-retract-until-point))
+    (progn
+      (save-excursion
+        (if (proof-only-whitespace-to-locked-region-p)
+            (proof-assert-next-command-interactive)
+          (proof-assert-until-point)))
+      (proof-maybe-follow-locked-end))))
+
+(defun my-coq-jump-to-definition ()
+  (interactive)
+  (coq-Print)
+  (execute-kbd-macro (kbd "RET")))
+
+(defun my-coq-docs ()
+  (interactive)
+  (coq-Check)
+  (execute-kbd-macro (kbd "RET")))
+
+(add-hook 'coq-mode-hook
+          '(lambda ()
+             (set (make-local-variable 'electric-indent-chars) '(?\n ?| ?.))
+             (define-key coq-mode-map k-eval 'move-proof-to-point)
+             (define-key coq-mode-map (kbd "<M-C-return>") 'proof-undo-last-successful-command)
+             (define-key coq-mode-map k-compile 'coq-Compile)
+             (define-key coq-mode-map k-jump-to-definition 'my-coq-jump-to-definition)
+             (define-key coq-mode-map k-docs 'my-coq-docs)
+             (define-key coq-mode-map k-apropos 'coq-SearchAbout)
+             (define-key coq-mode-map (kbd "M-p") 'coq-Print)
+             (define-key coq-mode-map (kbd "M-c") 'coq-Check)
+             (define-key coq-mode-map (kbd "M-l") 'proof-layout-windows)
+             ))
+
+(setq proof-follow-mode 'followdown)
+(setq proof-splash-enable nil)
 
 ;; IDRIS
 (setq idris-interpreter-path "/Users/eldar/Library/Haskell/bin/idris")
